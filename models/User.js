bbd80b6d496add177971c8ee05ef70dfdbc22209
sjy <stseo012@gmail.com>
2021-04-12 13:04:47 +0900

    비밀번호 암호화기능 추가 bcrypt

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -9,6 +10,10 @@ const userSchema = mongoose.Schema({
         type: String,
         trim: true,
         unique: 1
+    },
+    password: {
+        type: String,
+        minlength:5
     },
     lastname: {
         type: String,
@@ -23,10 +28,29 @@ const userSchema = mongoose.Schema({
         type: String
     },
     tokenExp: {//토큰의 유효기간
-        type:Number
+        type: Number
     }
 })
 
-const User = mongoose.model('User',userSchema)
+userSchema.pre('save', function (next) {
+    var user = this;
+    
+    if(user.isModified('password')){//user 정보중 password바뀔때만 실행
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err)
+    
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err)
+                user.password = hash
+                next()
+    
+            })
+            //비밀번호를 암호화 시킨다
+        })
+    }
+    
+}) //유저 모델에 정보를 저장하기 전에 
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = { User }
